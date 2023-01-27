b11e62db463342b75d35f27e19d5c7fee3cc5304
IlyaChistov <gaplis322@gmail.com>
2023-01-27 17:22:42 +0300

    Homework  3 change

--- Seminars/units/Civilian.java
+++ Seminars/units/Civilian.java
@@ -1,7 +1,5 @@
 package Seminars.units;
 
-import java.util.ArrayList;
-
 public class Civilian extends BaseHero{
 
     protected boolean supply;
@@ -9,15 +7,6 @@ public class Civilian extends BaseHero{
     public Civilian(String name, String role, int attack, int defence, int[] damage, int health, int speed, boolean supply) {
         super(name, role, attack, defence, damage, health, speed);
         this.supply = supply;
-    }
-    
-    @Override
-    public void step(ArrayList<BaseHero> heroList) {
-        if (supply == false){
-            supply = true;
-            System.out.println("Фермер по имени " + this.getName() + " снова стал активен.");
-        }
-        
     }
     
     @Override
--- Seminars/units/Farmer.java
+++ Seminars/units/Farmer.java
@@ -1,8 +1,19 @@
 package Seminars.units;
 
+import java.util.ArrayList;
+
 public class Farmer extends Civilian {
 
     public Farmer(String name) {
         super(name, "Farmer", 1, 1, new int[]{1, 1}, 1, 3, true);
+    }
+
+    @Override
+    public void step(ArrayList<BaseHero> heroList) {
+        if (supply == false){
+            supply = true;
+            System.out.println("Фермер по имени " + this.getName() + " снова стал активен.");
+        }
+        
     }
 }
