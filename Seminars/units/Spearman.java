f43226c82baae046b4944e6b37e21c9d82a0ae0f
IlyaChistov <gaplis322@gmail.com>
2023-01-31 17:22:36 +0300

    Homework 4 change

--- Seminars/units/Spearman.java
+++ Seminars/units/Spearman.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 
 public class Spearman extends Melee{
     public Spearman(ArrayList<BaseHero> teamList, int x, int y) {
-        super(teamList, "Sniper", 4, 5, new int [] {1, 3}, 10, 4, x, y);
+        super(teamList, "Spearman", 4, 5, new int [] {1, 3}, 10, 4, x, y);
     }
 }
